671ee7744e920e3144a6b6cf13e46564a41c1e18
Hyunsoo Lee <zcarc@naver.com>
2024-09-13 03:47:20 +0900

    .

---
+++ src/main/java/pojo/sequence/SequenceGenerator.java
@@ -0,0 +1,33 @@
+package pojo.sequence;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SequenceGenerator {
+
+    private final AtomicInteger counter = new AtomicInteger();
+    private String prefix;
+    private String suffix;
+    private int initial;
+
+    public SequenceGenerator() {
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    public void setInitial(int initial) {
+        this.initial = initial;
+    }
+
+    public String getSequence() {
+        return prefix
+                + initial
+                + counter.getAndIncrement()
+                + suffix;
+    }
+}
