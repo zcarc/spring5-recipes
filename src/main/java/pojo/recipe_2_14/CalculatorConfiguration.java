22c8fb0deaab064c2502adac5198f2dad1d96d90
Hyunsoo Lee <zcarc@naver.com>
2024-10-02 08:49:30 +0900

    레시피 2-14 조인포인트 정보 가져오기

---
+++ src/main/java/pojo/recipe_2_14/CalculatorConfiguration.java
@@ -0,0 +1,11 @@
+package pojo.recipe_2_14;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan
+public class CalculatorConfiguration {
+}
---
+++ src/main/java/pojo/recipe_2_14/UnitCalculator.java
@@ -0,0 +1,8 @@
+package pojo.recipe_2_14;
+
+public interface UnitCalculator {
+
+    public double kilogramToPound(double kilogram);
+
+    public double kilometerToMile(double kilometer);
+}
